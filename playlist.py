644e32e7f9fc3900434c12d4a4d35b206c9029fc
pieterjan <spruitje1>
2018-06-20 23:55:39 +0200

    add basic playlist support

--- playlist.py
+++ playlist.py
@@ -29,7 +29,7 @@ def get_items():
         playlists = get_playlists()
         # iterate over all playlists
         for playlist in playlists:
-            items = _rpc.request('Playlist.GetItems',{'playlistid': playlist['playlistid']})
+            items = _rpc.request('Playlist.GetItems', {'playlistid': playlist['playlistid']})
             # check if playlist is empty
             if 'items' in items:
                 # append playlist type
