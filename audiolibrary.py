6fff05ecbcd340567c09899a94c8bcca42e901a1
pieterjan <spruitje1>
2018-07-23 20:03:28 +0200

    add kodi.audiolibrary.get_albums

--- audiolibrary.py
+++ audiolibrary.py
@@ -1,6 +1,21 @@
 from . import rpc as _rpc
 
 
+def clean():
+    """
+    Cleans the audio library from non-existent items
+    """
+    _rpc.request('AudioLibrary.Clean')
+
+
+def scan():
+    """
+    Scans the audio sources for new library items
+    """
+    _rpc.request('AudioLibrary.Scan')
+
+
+
 def get_artists():
     """
     Retrieve all artists. For backward compatibility by default
@@ -10,3 +25,14 @@ def get_artists():
     """
     artists = _rpc.request('AudioLibrary.GetArtists')
     return artists['artists']
+
+
+def get_albums(artistid=None):
+    """
+    Retrieve all albums from specified artist (and role) or that has songs of the specified genre
+    """
+    if artistid:
+        albums = _rpc.request('AudioLibrary.GetAlbums',{'filter': {'artistid': artistid}})
+    else:
+        albums = _rpc.request('AudioLibrary.GetAlbums')
+    return albums['albums']
